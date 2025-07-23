cce6d4a75667e25994a7400ee457246d20f22b75
Umer Farooque <mohranvi4484@gmail.com>
2025-07-23 22:12:56 +0500

    Completed

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-
-var passwordBox = document.getElementById("password");
-var range = document.getElementById("slider");
-var passwordLength = document.getElementById("passwordLength");
-var rangeNum;
-function rangeUpdate(){
-    rangeNum = range.value;
-    passwordLength.innerHTML = "Password Length: " + rangeNum;
-}
-
 var eye = document.getElementById("eye");
 var eye_slash = document.getElementById("eye_slash");
-
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var symbols = "`~!@#$%^&*()_+-=|[]:{};'?<>,./'";
-
-var allChars = upperCase + lowerCase + numbers + symbols;
 function showPassword(){
     eye.style.display = "none";
     eye_slash.style.display = "block";
@@ -27,19 +10,44 @@ function hidePassword(){
     eye_slash.style.display = "none";
     passwordBox.type = "password";
 }
-function generatePassword(){
-    var password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.rangeNum)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.rangeNum)];
-    password += numbers[Math.floor(Math.random() * numbers.rangeNum)];
-    password += symbols[Math.floor(Math.random() * symbols.rangeNum)];
-
-    while(rangeNum > password.rangeNum){
-        password += allChars[Math.floor(Math.random() * allChars.rangeNum)];
-    }
-    passwordBox.value = password;
+var passwordBox = document.getElementById("password");
+var range = document.getElementById("slider");
+var passwordLength = document.getElementById("passwordLength");
+var rangeNum;
+function rangeUpdate(){
+    rangeNum = range.value;
+    passwordLength.innerHTML = "Password Length: " + rangeNum;
 }
-
+function generatePassword(){
+    var Length = rangeNum
+    var match = true
+    var includeUpperCase = document.getElementById("upperCase").checked
+    var includeLowerCase = document.getElementById("lowerCase").checked
+    var includeNumber = document.getElementById("num").checked
+    var includeSymbols = document.getElementById("symbols").checked
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "0123456789";
+    var symbols = "`~!@#$%^&*()_+-=|[]:{};'?<>,./'";   
+    var allowChars = ''
+    var password = ''  
+    allowChars += includeUpperCase ? upperCase : ""
+    allowChars += includeLowerCase ? lowerCase : ""
+    allowChars += includeNumber ? numbers : ""
+    allowChars += includeSymbols ? symbols : ""
+    if( allowChars.length === 0){
+        password = "At least one check box should be checked "
+        match = false
+        
+    }
+    else if(match){    
+        for( i=0 ; i<Length; i++){
+            var randomNum = Math.floor(Math.random() * allowChars.length)
+            password += allowChars[randomNum]
+        }
+    }
+    passwordBox.value = password;  
+}
 function copyPassword(){
     passwordBox.select();
     document.execCommand("copy");
