289ee13e66ab2296bbb183105d970b875adf941c
Umer Farooque <mohranvi4484@gmail.com>
2025-07-23 19:49:30 +0500

    updated

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 
 var passwordBox = document.getElementById("password");
-var length = 12;
+var range = document.getElementById("slider");
+var passwordLength = document.getElementById("passwordLength");
+var rangeNum;
+function rangeUpdate(){
+    rangeNum = range.value;
+    passwordLength.innerHTML = "Password Length: " + rangeNum;
+}
+
+var eye = document.getElementById("eye");
+var eye_slash = document.getElementById("eye_slash");
 
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -8,21 +17,31 @@ var numbers = "0123456789";
 var symbols = "`~!@#$%^&*()_+-=|[]:{};'?<>,./'";
 
 var allChars = upperCase + lowerCase + numbers + symbols;
-
+function showPassword(){
+    eye.style.display = "none";
+    eye_slash.style.display = "block";
+    passwordBox.type = "text";
+}
+function hidePassword(){
+    eye.style.display = "block";
+    eye_slash.style.display = "none";
+    passwordBox.type = "password";
+}
 function generatePassword(){
     var password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += numbers[Math.floor(Math.random() * numbers.length)];
-    password += symbols[Math.floor(Math.random() * symbols.length)];
+    password += upperCase[Math.floor(Math.random() * upperCase.rangeNum)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.rangeNum)];
+    password += numbers[Math.floor(Math.random() * numbers.rangeNum)];
+    password += symbols[Math.floor(Math.random() * symbols.rangeNum)];
 
-    while(length > password.length){
-        password += allChars[Math.floor(Math.random() * allChars.length)];
+    while(rangeNum > password.rangeNum){
+        password += allChars[Math.floor(Math.random() * allChars.rangeNum)];
     }
     passwordBox.value = password;
 }
 
 function copyPassword(){
     passwordBox.select();
-    document.execCommand("copy")
+    document.execCommand("copy");
+    passwordBox.value = "Copied!";
 }
